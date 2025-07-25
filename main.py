a408c7f98fa4c2f6b5b9a8e03aa505bd4ce06bf9
Huang-Hao-Gao <huanggao73@outlook.com>
2025-07-25 11:56:00 +0100

    stock price fluctuates slowly upwards

--- main.py
+++ main.py
@@ -2,13 +2,20 @@ import random as ran
 
 
 init = 100
-years = 5
-days = years * 365
-final = init
+years = 1
+days = years * 10
+cur = init
+percents = [x/10 for x in range(-20, 41, 1)]
+
+
 
 for day in range(days):
-    final += ran.randint(-10, 10)
+    change = (1+ran.choices(percents, k=1)[0]/100)
+    cur = cur * change
+    print("percent:" + str(change) + "\ncurrent:" + str(cur))
+    print("\n\n")
+    
 
 
 print("Initial Stock Price:" + str(init))
-print("Final Stock Price:" + str(final))
+print("Final Stock Price:" + str(round(cur, 2)))
