4b626cb960ea3f1bad92dc07599742f663bc8520
Huang-Hao-Gao <huanggao73@outlook.com>
2025-07-25 11:25:09 +0100

    stock price completely random

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-strt_val = 100
+import random as ran
+
+
+init = 100
+years = 5
+days = years * 365
+final = init
+
+for day in range(days):
+    final += ran.randint(-10, 10)
+
+
+print("Initial Stock Price:" + str(init))
+print("Final Stock Price:" + str(final))
